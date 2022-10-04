8815ce25ead465a65c7cbe21782307345afa3561
Alejandro Proia <62100665+Aleeproia@users.noreply.github.com>
2022-10-04 13:48:42 -0300

    Update ClienteDeCorreo.java

--- Practica- Orientacino a objetos 1/src/main/java/Ejercicio13/ClienteDeCorreo.java
+++ Practica- Orientacino a objetos 1/src/main/java/Ejercicio13/ClienteDeCorreo.java
@@ -4,17 +4,27 @@ import java.util.List;
 
 public class ClienteDeCorreo {
 	
-	private Carpeta inbox;
+	private Carpeta inbox = new Carpeta("Recibidos");
 	private List<Carpeta> carpetas;
 	
+	public ClienteDeCorreo() {
+		carpetas.add(inbox);
+	}
+	
 	public void recibir(Email email) {
-		this.inbox.agregarInbox(email);
+		this.inbox.agregarMail(email);
 	}
 	
 	public void mover(Email email,Carpeta origen,Carpeta destino) {
 		origen.quitar(email);
-		destino.agregarInbox(email);
+		destino.agregarMail(email); // destino.getEmails().add(email);
 	}
 	
-	
+	public Email buscar(String texto) {
+		return carpetas.stream().map( c -> c.buscarEmail(texto))
+				.filter(c -> c != null)
+				.findFirst() // tira excepcion si el elemento es null. hay que asegurarse que no lo sean. por eso filtro antes 
+				.orElse(null);
+		//return carpetas.stream().map( c -> c.getEmails().stream().map(e -> e.getTexto().contains(texto)))
+	}
 }
