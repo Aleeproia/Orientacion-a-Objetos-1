ccbc29a256fa964beadd6df707f0373e83063bf7
Alejandro Proia <62100665+Aleeproia@users.noreply.github.com>
2023-06-05 23:17:05 -0300

    Delete Ejercicio19-MercadoDeObjetos directory

--- Ejercicio19-MercadoDeObjetos/src/main/java/ar/edu/unlp/info/oo1/Ejercicio19_MercadoDeObjetos/Envio.java
+++
@@ -1,7 +0,0 @@
-package ar.edu.unlp.info.oo1.Ejercicio19_MercadoDeObjetos;
-
-public abstract class Envio {
-	
-	public abstract double calcularEnvio();
-
-}
--- Ejercicio19-MercadoDeObjetos/src/main/java/ar/edu/unlp/info/oo1/Ejercicio19_MercadoDeObjetos/Pago.java
+++
@@ -1,7 +0,0 @@
-package ar.edu.unlp.info.oo1.Ejercicio19_MercadoDeObjetos;
-
-public abstract class Pago {
-	
-	public abstract double calcularPago(Producto producto);
-
-}
